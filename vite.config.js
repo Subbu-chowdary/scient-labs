661e247a962fce280cf19cb86f7bcffc379addf3
Kethineni venkata subbaiah chowdary <129073951+Subbu-chowdary@users.noreply.github.com>
2025-07-02 12:57:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/scientlabs/", // <-- add this line
+  base: "/scient-labs/", // <-- add this line
 });
